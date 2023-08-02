40fcc50a73349b645cfe90d7c288e384a6b7cbff
Michael B <mjbradvica@gmail.com>
2023-08-02 18:44:13 -0400

    started chain objects

---
+++ source/ChainStrategy/ChainRequest.cs
@@ -0,0 +1,34 @@
+﻿namespace ChainStrategy
+{
+    /// <summary>
+    /// An abstract chain request that has basic properties and methods for a chain process.
+    /// </summary>
+    public abstract class ChainRequest : IChainRequest
+    {
+        /// <inheritdoc/>
+        public bool IsFaulted { get; private set; }
+
+        /// <summary>
+        /// Gets the exception that may have occurred in the chain.
+        /// </summary>
+        public Exception? Exception { get; private set; }
+
+        /// <summary>
+        /// A method that designates the chain has faulted during its execution.
+        /// </summary>
+        public virtual void Faulted()
+        {
+            IsFaulted = true;
+        }
+
+        /// <summary>
+        /// A method that designates the chain has faulted during its execution with an exception.
+        /// </summary>
+        /// <param name="exception">A <see cref="Exception"/> that occurred during the chain process.</param>
+        public virtual void Faulted(Exception exception)
+        {
+            IsFaulted = true;
+            Exception = exception;
+        }
+    }
+}
---
+++ source/ChainStrategy/IChainRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainStrategy
+{
+    /// <summary>
+    /// The basic interface for a request object in a chain of responsibility.
+    /// </summary>
+    public interface IChainRequest
+    {
+        /// <summary>
+        /// Gets a value indicating whether the chain has faulted.
+        /// </summary>
+        bool IsFaulted { get; }
+    }
+}
